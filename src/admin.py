1b500797a511614309164e8d667ab5053b0b1730
Leonardo Barros <leonprogramer@gmail.com>
2024-05-12 20:29:26 -0300

    feat: modify of src to raffle app

--- src/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import AutomaticBuy, Category, Image, Raffle
-
-
-admin.site.register(AutomaticBuy)
-admin.site.register(Category)
-admin.site.register(Image)
-admin.site.register(Raffle)
