2cd69fc6c407331d0b1983562255ee821592bdaa
Leonardo Barros <leonprogramer@gmail.com>
2024-05-27 09:57:48 -0300

    feat: Order, OrderItem and ShippingAddress registereds

--- store/admin.py
+++ store/admin.py
@@ -1,7 +1,6 @@
 from django.contrib import admin
-from .models import Cart, CartItem, Order, OrderItem
+from .models import Order, OrderItem, ShippingAddress
 
-admin.site.register(Cart)
-admin.site.register(CartItem)
 admin.site.register(Order)
 admin.site.register(OrderItem)
+admin.site.register(ShippingAddress)
