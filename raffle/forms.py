a7e5099281bb1aecdf83d749a452982971505f15
Leonardo Barros <leonprogramer@gmail.com>
2024-06-05 23:44:23 -0300

    feat: formset implementation realized to raffle

--- raffle/forms.py
+++ raffle/forms.py
@@ -10,17 +10,28 @@ class_default = 'form-control mb-3'
 class RaffleForm(forms.ModelForm):
 	class Meta:
 		model = Raffle
-		fields = '__all__'
-		exclude = ['owner']
+		fields = ['title', 'scheduled_date', 'number_quantity', 'category', 'price', 'min_quantity', 'digital', 'description', 'owner']
+		# exclude = ['owner']
+		labels = {
+			'title': 'Nome da Rifa',
+			'scheduled_date': 'Data',
+			'number_quantity': 'Quantidade de números',
+			'category': 'Catégorias',
+			'price': 'Preço',
+			'min_quantity': 'Quantidade mínima',
+			'digital': 'Typo de produto',
+			'description': 'Descrção',
+		}
 		widgets = {
 			'title': forms.TextInput(attrs={'class': class_default}),
 			'scheduled_date': forms.DateTimeInput(attrs={'type': 'datetime-local', 'class': class_default}),
 			'number_quantity': forms.Select(attrs={'class': class_default}),
+			'category': forms.Select(attrs={'class': 'form-control mb-3'}),
 			'price': forms.TextInput(attrs={'class': class_default}),
 			'min_quantity': forms.NumberInput(attrs={'class': class_default}),
 			'digital': forms.NullBooleanSelect(attrs={'class': class_default}),
-			'description': forms.Textarea(attrs={'class': class_default}),
-			'category': forms.SelectMultiple(attrs={'class': 'form-select mb-3', 'size': '3'}),
+			'description': forms.Textarea(attrs={'class': class_default, 'rows': 3, 'cols': 50, 'aria-describedby': 'Descrição de Rifa'}),
+			'owner': forms.TextInput(attrs={'hidden': True})
 		}
 
 
@@ -91,10 +102,12 @@ AutomaticBuyFormSet = inlineformset_factory(
 	fields=['quantity', 'more_popular'],
 	widgets = {
 		'quantity': forms.NumberInput(
-			attrs={'class': class_default, 'id': 'radio1'}),
+			attrs={'class': class_default}),
 		'more_popular': forms.TextInput(
-			attrs={'type': 'radio', 'class': 'form-check-input mb-3', 'id': 'radio1'}),
-	}
+			attrs={'type': 'radio', 'class': 'form-check-input mb-3'}),
+	},
+	extra=4,
+	can_delete=False
 )
 
 PromotionFormSet = inlineformset_factory(
@@ -106,7 +119,9 @@ PromotionFormSet = inlineformset_factory(
 			attrs={'type': 'number', 'class': class_default}),
 		'price': forms.TextInput(
 			attrs={'class': 'form-control'}),
-	}
+	},
+	extra=1,
+	can_delete=False
 )
 
 AwardedQuotaFormSet = inlineformset_factory(
@@ -116,5 +131,7 @@ AwardedQuotaFormSet = inlineformset_factory(
 	widgets = {
 		'number': forms.NumberInput(
 			attrs={'type': 'number', 'class': class_default}),
-	}
+	},
+	extra=1,
+	can_delete=False
 )
