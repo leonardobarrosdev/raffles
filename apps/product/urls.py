1121fcb66b433a45ca39c9cdef2eb64458dd871a
Leonardo Barros <leonprogramer@gmail.com>
2024-10-08 21:24:07 -0300

    feat: updated

--- apps/product/urls.py
+++ apps/product/urls.py
@@ -4,9 +4,8 @@ from . import views
 app_name = 'product'
 
 urlpatterns = [
-	path('list/', views.list, name='list'),
-	path('create/', views.CreateView.as_view(), name='create'),
-	path('<int:id>/', views.details, name='details'),
-	path('<int:id>/delete/', views.delete, name='delete'),
-	re_path(r'^(?P<id>[0-9]{1,4})/update(?:/(?P<image_id>[0-9]{1,5}))?/$', views.UpdateView.as_view(), name='update'),
+	path('list/', views.product_list, name='list'),
+	path('create/', views.ProductCreateView.as_view(), name='create'),
+	path('<int:id>/', views.product_details, name='details'),
+	path('<int:id>/delete/', views.product_delete, name='delete'),
 ]
