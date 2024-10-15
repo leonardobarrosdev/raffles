5d0d37c1a56ddd1bcd463637b18f979249b6eaa9
Leonardo Barros <leonprogramer@gmail.com>
2024-10-15 20:36:23 -0300

    feat: deleted unecessary field (customer)

--- apps/raffle/models.py
+++ apps/raffle/models.py
@@ -1,10 +1,8 @@
 from django.db import models
-from django.contrib.auth import get_user_model
 from apps.product.models import Product
 
 
 class Raffle(models.Model):
-	customer = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
 	product = models.ForeignKey(Product, on_delete=models.CASCADE)
 	number = models.PositiveIntegerField()
 
